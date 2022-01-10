dbc44724b90155e0827dfc6f73d4ab8c28255c7b
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2022-01-10 23:21:07 +0600

    Initial Commit

---
+++ Doodle-Country-Practice-Test/Doodle_Country_Practice_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doodle_Country_Practice_TestApp.swift
+//  Doodle-Country-Practice-Test
+//
+//  Created by Shakhawat Hossain Shahin on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Doodle_Country_Practice_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
